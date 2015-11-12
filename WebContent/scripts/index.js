e6d483adb4c4d454fee6181a8b90bdf8df2477a4
mfleish <derekmatt333@aol.com>
2015-11-11 21:23:31 -0500

    UI updates: added popups for table cells, table can now scroll, sections for group details, input, and table

--- WebContent/scripts/index.js
+++ WebContent/scripts/index.js
@@ -1,7 +1,13 @@
+var aPopup1 = '<a style="position:inherit;" href="#infoPopup1" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all" data-position-to="window">Yes</a>';
+var aPopup2 = '<a style="position:inherit;" href="#infoPopup2" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all" data-position-to="window">Yes</a>';
+var aPopup3 = '<a style="position:inherit;" href="#infoPopup3" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all" data-position-to="window">Yes</a>';
+var aPopup4 = '<a style="position:inherit;" href="#infoPopup4" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all" data-position-to="window">Yes</a>';
+var aPopup5 = '<a style="position:inherit;" href="#infoPopup5" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all" data-position-to="window">Yes</a>';
+
 $( document ).ready(function() {
     
 	document.getElementById('domainName').value = "https://paypal.com";
-	document.getElementById('numberPages').value = "10";
+	document.getElementById('numberPages').value = "2";
 	document.getElementById('btnResults').style.visibility = "hidden";
 	document.getElementById('loader').style.visibility = "hidden";
 	
@@ -19,7 +25,16 @@ $( document ).ready(function() {
 function addUrlsTable(data) {
 	for (var index in data.urls) {
 	    var urlName = data.urls[index];
-	    $('#tableMain').append('<tr><td>'+urlName+'</td><td></td><td></td><td></td><td></td><td></td></tr>');
+	    
+	  //Here is where vulnerability text should assigned to each popup
+	    document.getElementById('infoPopup1').innerHTML = "Content-Security Policy";
+	    document.getElementById('infoPopup2').innerHTML = "HTTP Strict Transport Policy";
+	    document.getElementById('infoPopup3').innerHTML = "HttpOnly and Secure Cookies";
+	    document.getElementById('infoPopup4').innerHTML = "Anticlickjacking headers (X-Frame-Options)";
+	    document.getElementById('infoPopup5').innerHTML = "Nonces in web forms";
+	    
+	    $('#tableMain').append('<tr><td>'+urlName+'</td><td>'+aPopup1+'</td><td>'+aPopup2+'</td><td>'+ aPopup3 +
+	    		'</td><td>'+aPopup4+'</td><td>'+aPopup5+'</td></tr>');
 	}
 }
 
